6856bc964ed7298b6ebb7211bf6577c893ae70e2
marcinm <mm.kodmax@gmail.com>
2023-06-12 23:18:26 +0200

    gpiod_chip open close

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import bind from 'bindings'
-
 const libgpiod = bind('gpiod.node')
 
+const chip: unknown = libgpiod.chip_open_by_name('gpiochip0')
 
-console.log(libgpiod.hello())
+libgpiod.blink(chip)
+
+libgpiod.chip_close(chip)
--- src/libgpiod.cc
+++ src/libgpiod.cc
@@ -1,10 +1,11 @@
-#include <nan.h>
 #include <gpiod.h>
+#include <nan.h>
 #include <stdio.h>
 #include <unistd.h>
 
 namespace libgpiod {
-
+using v8::Exception;
+using v8::External;
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
@@ -12,27 +13,62 @@ using v8::Object;
 using v8::String;
 using v8::Value;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
+void chip_open_by_name(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
 
-   const char *chipname = "gpiochip0";
-   struct gpiod_chip *chip;
-   struct gpiod_line *lineRed;    // Red LED
-   struct gpiod_line *lineGreen;  // Green LED
-   struct gpiod_line *lineYellow; // Yellow LED
-   struct gpiod_line *lineButton; // Pushbutton
-   int i, val;
+  if (args.Length() != 1 || !args[0]->IsString()) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+    return;
+  }
 
-   // Open GPIO chip
-   chip = gpiod_chip_open_by_name(chipname);
+  String::Utf8Value chipname(isolate, args[0]);
+  struct gpiod_chip *chip;
 
-//   // Open GPIO lines
+  chip = gpiod_chip_open_by_name(*chipname);
+
+  args.GetReturnValue().Set(External::New(isolate, chip));
+}
+
+void chip_close(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+
+  if (args.Length() != 1 || !args[0]->IsExternal()) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+    return;
+  }
+
+  gpiod_chip_close((gpiod_chip *)External::Cast(*args[0])->Value());
+}
+
+void blink(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+
+  if (args.Length() != 1 || !args[0]->IsExternal()) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+    return;
+  }
+
+
+  // const char *chipname = "gpiochip0";
+
+  struct gpiod_chip *chip = (gpiod_chip *)External::Cast(*args[0])->Value();
+  struct gpiod_line *lineRed;    // Red LED
+  struct gpiod_line *lineGreen;  // Green LED
+  struct gpiod_line *lineYellow; // Yellow LED
+  struct gpiod_line *lineButton; // Pushbutton
+  int i, val;
+
+  // Open GPIO chip
+  // chip = gpiod_chip_open_by_name(chipname);
+
+  //   // Open GPIO lines
   lineGreen = gpiod_chip_get_line(chip, 17);
   lineButton = gpiod_chip_get_line(chip, 27);
 
-//   // Open LED lines for output
+  //   // Open LED lines for output
   gpiod_line_request_output(lineGreen, "example1", 0);
 
-//   // Open switch line for input
+  //   // Open switch line for input
   gpiod_line_request_input(lineButton, "example1");
 
   // Blink LEDs in a binary pattern
@@ -52,18 +88,20 @@ void Method(const FunctionCallbackInfo<Value>& args) {
   // Release lines and chip
   gpiod_line_release(lineGreen);
   gpiod_line_release(lineButton);
-   gpiod_chip_close(chip);
+  // gpiod_chip_close(chip);
 
-
-  Isolate* isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(
-      isolate, "world").ToLocalChecked());
+                                isolate, "world")
+                                .ToLocalChecked());
 }
 
-void Initialize(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
+void initialize(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "chip_open_by_name", chip_open_by_name);
+  NODE_SET_METHOD(exports, "chip_close", chip_close);
+
+  NODE_SET_METHOD(exports, "blink", blink);
 }
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+NODE_MODULE(NODE_GYP_MODULE_NAME, initialize)
 
-}  // namespace demo 
+} // namespace libgpiod
