9918790ed89be7549bc0ec3695e60c887ee01ff3
marcinm <mm.kodmax@gmail.com>
2023-06-23 23:16:28 +0200

    removed docs from npm publish

--- install.ts
+++ install.ts
@@ -43,7 +43,7 @@ const isSuitableEnvironment = async () => {
                     } else {
                         resolve()
                     }
-                })    
+                })
             })
         })
 
